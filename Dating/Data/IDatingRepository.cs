4bef9df4970911d4915ee9b09982f2732925713c
Kevin Stradtman <stradtkt22@gmail.com>
2020-07-06 01:30:40 -0400

    Added seeded data to the backend of the application

---
+++ Dating/Data/IDatingRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dating.Models;
+
+namespace Dating.Data
+{
+    public interface IDatingRepository
+    {
+        void Add<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+    }
+}
--- Dating/Startup.cs
+++ Dating/Startup.cs
@@ -38,6 +38,7 @@ namespace Dating
             services.AddControllers();
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
