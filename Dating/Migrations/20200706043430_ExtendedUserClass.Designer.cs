88c839d9b8de9752b8cfc8c43c9f0d18795278ef
Kevin Stradtman <stradtkt22@gmail.com>
2020-07-06 00:36:46 -0400

    Extended the user model

--- Dating/Migrations/20200706043430_ExtendedUserClass.Designer.cs
+++ Dating/Migrations/20200706043430_ExtendedUserClass.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Dating.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dating.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200706043430_ExtendedUserClass")]
+    partial class ExtendedUserClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
