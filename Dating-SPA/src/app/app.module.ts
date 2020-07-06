25d91d0a5f731551ed97c47e6ec9cf0c8576ac6f
Kevin Stradtman <stradtkt22@gmail.com>
2020-07-06 05:03:00 -0400

    Added the view for the members and the buttons on the cards

--- Dating-SPA/src/app/app.module.ts
+++ Dating-SPA/src/app/app.module.ts
@@ -12,10 +12,11 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './services/error.interceptor';
 import { AlertifyService } from './services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 
 
@@ -27,7 +28,8 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent
    ],
    imports: [
       BrowserModule,
